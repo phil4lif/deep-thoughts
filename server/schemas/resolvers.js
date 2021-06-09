7130da35c6b6a0b773d432cb05ff4588c6155015
phil cowan <philipjcowan1@gmail.com>
2021-06-09 16:22:57 -0700

    typedef for thoughts and thought and query to get all

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,9 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        thoughts: async () => {
+            return Thought.find().sort({ createdAt: -1 });
         }
     }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,15 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+    }
     type Query {
-        helloWorld: String
+        thoughts: [Thought]
     }
 `;
 
