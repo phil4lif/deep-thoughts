b48ab54c31462811ab60561fa8c40e68c8e8bab4
phil cowan <philipjcowan1@gmail.com>
2021-06-12 10:53:56 -0700

    addss resolver for login along with authentication error handler from apollo

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
@@ -23,12 +24,22 @@ const resolvers = {
         }
     }, 
     Mutation: {
-        addUser: async () => {
+        addUser: async (parent, args) => {
             const user = await User.create(args);
             return user;
         },
-        login: async () => {
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({ email });
 
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            return user;
         }
     }
 };
