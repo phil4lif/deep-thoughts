3a00f97708ee888ffcaf5fb439623d2883d26fbb
phil cowan <philipjcowan1@gmail.com>
2021-06-15 09:40:33 -0700

    adds reusable friendsList component and displays currently only on profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
+import FriendList from '../components/FriendList';
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -31,7 +32,13 @@ const Profile = () => {
         <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
         </div>
 
-        <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}</div>
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+          username={user.username}
+          friendCount={user.friendCount}
+          friends={user.friends}
+          />
+        </div>
       </div>
     </div>
   );
