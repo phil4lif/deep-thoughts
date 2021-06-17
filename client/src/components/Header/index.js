3b64caff56b66ecd810c113f2f6c1ee79d49ad63
phil cowan <philipjcowan1@gmail.com>
2021-06-16 17:21:54 -0700

    adds more profile user enhancements

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
+
+  const logout = e => {
+    e.preventDefault();
+    Auth.logout();
+  }
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -9,8 +15,17 @@ const Header = () => {
           <h1>Deep Thoughts</h1>
         </Link>
         <nav className="text-center">
-          <Link to='/login'>Login</Link>
-          <Link to='/signup'>Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href='/' onClick={logout}>Logout</a>
+            </>
+          ) : (
+            <>
+              <Link to='/login'>Login</Link>
+              <Link to='/signup'>Signup</Link></>
+          )}
+
         </nav>
       </div>
     </header>
